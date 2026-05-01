b2eb27d3ad3899bedb2ac8aa8a1a342e9c9372cc
Nitish Kr <pnnitish88725@gmail.com>
2026-05-01 20:07:57 +0530

    core fix

--- src/server.js
+++ src/server.js
@@ -39,7 +39,7 @@ app.use(
         return callback(null, true);
       }
 
-      return callback(null, true); // 🔥 TEMP allow all to debug
+      return callback(new Error("Not allowed by CORS"));
     },
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
